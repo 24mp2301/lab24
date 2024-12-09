30b13101b0b63e674a1d903307f82d0f19f98215
Jyo Jokooz <24mp2301@rit.ac.in>
2024-12-09 15:19:15 +0530

    Update 2_pgm.py

--- lab/24mca34/pylab/cycle5/2_pgm.py
+++ lab/24mca34/pylab/cycle5/2_pgm.py
@@ -1,19 +1,10 @@
 import datetime
-
 now = datetime.datetime.now()
-
 print("a) Current Date and Time:", now)
-
 print("b) Current Year:", now.year)
-
 print("c) Month of the Year:", now.month)
-
 print("d) Week Number of the Year:", now.isocalendar()[1])
-
-print("e) Weekday Number:", now.weekday())
-
+print("e) Weekday Number:", now.isoweekday())
 print("f) Day of the Year:", now.timetuple().tm_yday)
-
 print("g) Day of the Month:", now.day)
-
-print("h) Day of Week:", now.isoweekday())
+print("h) Day of Week:", now.strftime('%A'))
